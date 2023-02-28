9ba5a3538eba67cc5d13da01d289a2f59d99edda
LORENZODELL\rosin <rosin.lorenzo@hotmail.it>
2023-02-28 21:26:03 +0100

    Update

--- C/Src/CoreHL/Inc/eFSS_BLOBC.h
+++ C/Src/CoreHL/Inc/eFSS_BLOBC.h
@@ -48,8 +48,6 @@ typedef enum
 typedef struct
 {
     t_eFSS_COREHL_Ctx tCOREHLCtx;
-    bool_t bIsWriteOnGoing;
-    uint32_t uDataWritten;
 }t_eFSS_BLOBC_Ctx;
 
 
@@ -85,151 +83,18 @@ e_eFSS_BLOBC_RES eFSS_BLOBC_InitCtx(t_eFSS_BLOBC_Ctx* const p_ptCtx, t_eFSS_TYPE
 e_eFSS_BLOBC_RES eFSS_BLOBC_IsInit(t_eFSS_BLOBC_Ctx* const p_ptCtx, bool_t* p_pbIsInit);
 
 /**
- * @brief       Get the status of the storage
+ * @brief       Get the reference of buffer that we can use to read or write data from storage
  *
- * @param[in]   p_ptCtx    - Blob context
+ * @param[in]   p_ptCtx       - Log Core context
+ * @param[out]  p_ptBuff      - Pointer to a struct that will be filled with info about buffer
  *
- * @return      e_eFSS_BLOBC_RES_BADPOINTER         - In case of bad pointer passed to the function
- *              e_eFSS_BLOBC_RES_OK                 - Operation ended correctly
- *              e_eFSS_BLOBC_RES_OK_BKP_RCVRD       - All ok, but some page where recovered
- *              e_eFSS_BLOBC_RES_NOTVALIDBLOB       - No valid blob founded
- *              e_eFSS_BLOBC_RES_NEWVERSIONBLOB     - New version of the blob requested
- *              e_eFSS_BLOBC_RES_NOINITLIB          - Need to init the lib before calling this function
- *              e_eFSS_BLOBC_RES_CORRUPTCTX         - Context is corrupted
- *              e_eFSS_BLOBC_RES_CLBCKERASEERR      - Erase callback returned error
- *              e_eFSS_BLOBC_RES_CLBCKWRITEERR      - Write callback returned error
- *              e_eFSS_BLOBC_RES_CLBCKREADERR       - Read callback returned error
- *              e_eFSS_BLOBC_RES_CLBCKCRCERR        - Crc callback returned error
- *              e_eFSS_BLOBC_RES_WRITENOMATCHREAD   - After Write operation the Read operation readed different data
+ * @return      e_eFSS_BLOBC_RES_BADPOINTER    - In case of bad pointer passed to the function
+ *		        e_eFSS_BLOBC_RES_CORRUPTCTX    - Context is corrupted
+ *		        e_eFSS_BLOBC_RES_NOINITLIB     - Need to init lib before calling function
+ *              e_eFSS_BLOBC_RES_OK            - Operation ended correctly
  */
-e_eFSS_BLOBC_RES eFSS_BLOBC_GetStorageStatus(t_eFSS_BLOBC_Ctx* const p_ptCtx);
+e_eFSS_BLOBC_RES eFSS_BLOBC_GetBuff(t_eFSS_BLOBC_Ctx* p_ptCtx, t_eFSS_TYPE_StorBuf* p_ptBuff);
 
-/**
- * @brief       Get info about the stored blob
- *
- * @param[in]   p_ptCtx      - Blob context
- * @param[out]  p_puBlobSize - Pointer to a uint32_t that will be filled with the size of the blob
- *
- * @return      e_eFSS_BLOBC_RES_BADPOINTER         - In case of bad pointer passed to the function
- *              e_eFSS_BLOBC_RES_OK                 - Operation ended correctly
- *              e_eFSS_BLOBC_RES_OK_BKP_RCVRD       - All ok, but some page where recovered
- *              e_eFSS_BLOBC_RES_NOTVALIDBLOB       - No valid blob founded
- *              e_eFSS_BLOBC_RES_NEWVERSIONBLOB     - New version of the blob requested
- *              e_eFSS_BLOBC_RES_NOINITLIB          - Need to init the lib before calling this function
- *              e_eFSS_BLOBC_RES_CORRUPTCTX         - Context is corrupted
- *              e_eFSS_BLOBC_RES_CLBCKERASEERR      - Erase callback returned error
- *              e_eFSS_BLOBC_RES_CLBCKWRITEERR      - Write callback returned error
- *              e_eFSS_BLOBC_RES_CLBCKREADERR       - Read callback returned error
- *              e_eFSS_BLOBC_RES_CLBCKCRCERR        - Crc callback returned error
- *              e_eFSS_BLOBC_RES_WRITENOMATCHREAD   - After Write operation the Read operation readed different data
- */
-e_eFSS_BLOBC_RES eFSS_BLOBC_GetInfo(t_eFSS_BLOBC_Ctx* const p_ptCtx, uint32_t* p_puBlobSize);
-
-/**
- * @brief       Format the memory used for the blob and all data
- *
- * @param[in]   p_ptCtx    - Blob context
- *
- * @return      e_eFSS_BLOBC_RES_BADPOINTER         - In case of bad pointer passed to the function
- *              e_eFSS_BLOBC_RES_OK                 - Operation ended correctly
- *              e_eFSS_BLOBC_RES_OK_BKP_RCVRD       - All ok, but some page where recovered
- *              e_eFSS_BLOBC_RES_NOTVALIDBLOB       - No valid blob founded
- *              e_eFSS_BLOBC_RES_NEWVERSIONBLOB     - New version of the blob requested
- *              e_eFSS_BLOBC_RES_NOINITLIB          - Need to init the lib before calling this function
- *              e_eFSS_BLOBC_RES_CORRUPTCTX         - Context is corrupted
- *              e_eFSS_BLOBC_RES_CLBCKERASEERR      - Erase callback returned error
- *              e_eFSS_BLOBC_RES_CLBCKWRITEERR      - Write callback returned error
- *              e_eFSS_BLOBC_RES_CLBCKREADERR       - Read callback returned error
- *              e_eFSS_BLOBC_RES_CLBCKCRCERR        - Crc callback returned error
- *              e_eFSS_BLOBC_RES_WRITENOMATCHREAD   - After Write operation the Read operation readed different data
- */
-e_eFSS_BLOBC_RES eFSS_BLOBC_Format(t_eFSS_BLOBC_Ctx* const p_ptCtx);
-
-/**
- * @brief       Read the blob
- *
- * @param[in]   p_ptCtx    - Blob context
- * @param[in]   p_uOffset  - Offset where to start read the blob
- * @param[in]   p_puBuff   - Pointer to a buffer where to copy readed data
- * @param[in]   p_uBuffL   - Size of p_puBuff
- *
- * @return      e_eFSS_BLOBC_RES_BADPOINTER         - In case of bad pointer passed to the function
- *              e_eFSS_BLOBC_RES_OK                 - Operation ended correctly
- *              e_eFSS_BLOBC_RES_OK_BKP_RCVRD       - All ok, but some page where recovered
- *              e_eFSS_BLOBC_RES_NOTVALIDBLOB       - No valid blob founded
- *              e_eFSS_BLOBC_RES_NEWVERSIONBLOB     - New version of the blob requested
- *              e_eFSS_BLOBC_RES_NOINITLIB          - Need to init the lib before calling this function
- *              e_eFSS_BLOBC_RES_CORRUPTCTX         - Context is corrupted
- *              e_eFSS_BLOBC_RES_CLBCKERASEERR      - Erase callback returned error
- *              e_eFSS_BLOBC_RES_CLBCKWRITEERR      - Write callback returned error
- *              e_eFSS_BLOBC_RES_CLBCKREADERR       - Read callback returned error
- *              e_eFSS_BLOBC_RES_CLBCKCRCERR        - Crc callback returned error
- *              e_eFSS_BLOBC_RES_WRITENOMATCHREAD   - After Write operation the Read operation readed different data
- */
-e_eFSS_BLOBC_RES eFSS_BLOBC_ReadBlob(t_eFSS_BLOBC_Ctx* const p_ptCtx, uint32_t p_uOffset, uint8_t* p_puBuff,
-                                     uint32_t p_uBuffL);
-
-/**
- * @brief       Start to write the blob
- *
- * @param[in]   p_ptCtx    - Blob context
- *
- * @return      e_eFSS_BLOBC_RES_BADPOINTER         - In case of bad pointer passed to the function
- *              e_eFSS_BLOBC_RES_OK                 - Operation ended correctly
- *              e_eFSS_BLOBC_RES_OK_BKP_RCVRD       - All ok, but some page where recovered
- *              e_eFSS_BLOBC_RES_NOTVALIDBLOB       - No valid blob founded
- *              e_eFSS_BLOBC_RES_NEWVERSIONBLOB     - New version of the blob requested
- *              e_eFSS_BLOBC_RES_NOINITLIB          - Need to init the lib before calling this function
- *              e_eFSS_BLOBC_RES_CORRUPTCTX         - Context is corrupted
- *              e_eFSS_BLOBC_RES_CLBCKERASEERR      - Erase callback returned error
- *              e_eFSS_BLOBC_RES_CLBCKWRITEERR      - Write callback returned error
- *              e_eFSS_BLOBC_RES_CLBCKREADERR       - Read callback returned error
- *              e_eFSS_BLOBC_RES_CLBCKCRCERR        - Crc callback returned error
- *              e_eFSS_BLOBC_RES_WRITENOMATCHREAD   - After Write operation the Read operation readed different data
- */
-e_eFSS_BLOBC_RES eFSS_BLOBC_StartWrite(t_eFSS_BLOBC_Ctx* const p_ptCtx);
-
-/**
- * @brief       Append some data to the blob that we have started to write
- *
- * @param[in]   p_ptCtx    - Blob context
- * @param[in]   p_puBuff   - Pointer to a buffer that must be write in the storage area
- * @param[in]   p_uBuffL   - Size of p_puBuff
- *
- * @return      e_eFSS_BLOBC_RES_BADPOINTER         - In case of bad pointer passed to the function
- *              e_eFSS_BLOBC_RES_OK                 - Operation ended correctly
- *              e_eFSS_BLOBC_RES_OK_BKP_RCVRD       - All ok, but some page where recovered
- *              e_eFSS_BLOBC_RES_NOTVALIDBLOB       - No valid blob founded
- *              e_eFSS_BLOBC_RES_NEWVERSIONBLOB     - New version of the blob requested
- *              e_eFSS_BLOBC_RES_NOINITLIB          - Need to init the lib before calling this function
- *              e_eFSS_BLOBC_RES_CORRUPTCTX         - Context is corrupted
- *              e_eFSS_BLOBC_RES_CLBCKERASEERR      - Erase callback returned error
- *              e_eFSS_BLOBC_RES_CLBCKWRITEERR      - Write callback returned error
- *              e_eFSS_BLOBC_RES_CLBCKREADERR       - Read callback returned error
- *              e_eFSS_BLOBC_RES_CLBCKCRCERR        - Crc callback returned error
- *              e_eFSS_BLOBC_RES_WRITENOMATCHREAD   - After Write operation the Read operation readed different data
- */
-e_eFSS_BLOBC_RES eFSS_BLOBC_AppendData(t_eFSS_BLOBC_Ctx* const p_ptCtx, uint8_t* p_puBuff, uint32_t p_uBuffL);
-
-/**
- * @brief       Save all the data written till now
- *
- * @param[in]   p_ptCtx    - Blob context
- *
- * @return      e_eFSS_BLOBC_RES_BADPOINTER         - In case of bad pointer passed to the function
- *              e_eFSS_BLOBC_RES_OK                 - Operation ended correctly
- *              e_eFSS_BLOBC_RES_OK_BKP_RCVRD       - All ok, but some page where recovered
- *              e_eFSS_BLOBC_RES_NOTVALIDBLOB       - No valid blob founded
- *              e_eFSS_BLOBC_RES_NEWVERSIONBLOB     - New version of the blob requested
- *              e_eFSS_BLOBC_RES_NOINITLIB          - Need to init the lib before calling this function
- *              e_eFSS_BLOBC_RES_CORRUPTCTX         - Context is corrupted
- *              e_eFSS_BLOBC_RES_CLBCKERASEERR      - Erase callback returned error
- *              e_eFSS_BLOBC_RES_CLBCKWRITEERR      - Write callback returned error
- *              e_eFSS_BLOBC_RES_CLBCKREADERR       - Read callback returned error
- *              e_eFSS_BLOBC_RES_CLBCKCRCERR        - Crc callback returned error
- *              e_eFSS_BLOBC_RES_WRITENOMATCHREAD   - After Write operation the Read operation readed different data
- */
-e_eFSS_BLOBC_RES eFSS_BLOBC_EndWrite(t_eFSS_BLOBC_Ctx* const p_ptCtx);
 
 
 #ifdef __cplusplus
--- C/Src/CoreHL/Src/eFSS_BLOBC.c
+++ C/Src/CoreHL/Src/eFSS_BLOBC.c
@@ -81,11 +81,6 @@ e_eFSS_BLOBC_RES eFSS_BLOBC_InitCtx(t_eFSS_BLOBC_Ctx* const p_ptCtx, t_eFSS_TYPE
             /* Can init low level context */
             l_eResHL = eFSS_COREHL_InitCtx(&p_ptCtx->tCOREHLCtx, p_tCtxCb, p_tStorSet, p_puBuff, p_uBuffL);
             l_eRes = eFSS_DB_HLtoDBRes(l_eResHL);
-
-            if( e_eFSS_BLOBC_RES_OK == l_eRes )
-            {
-                /* Init internal context */
-            }
         }
     }
 
@@ -112,25 +107,25 @@ e_eFSS_BLOBC_RES eFSS_BLOBC_IsInit(t_eFSS_BLOBC_Ctx* const p_ptCtx, bool_t* p_pb
 	return l_eRes;
 }
 
-e_eFSS_BLOBC_RES eFSS_BLOBC_GetStorageStatus(t_eFSS_BLOBC_Ctx* const p_ptCtx)
+e_eFSS_BLOBC_RES eFSS_BLOBC_GetBuff(t_eFSS_BLOBC_Ctx* p_ptCtx, t_eFSS_TYPE_StorBuf* p_ptBuff)
 {
-	/* Local return variable */
+	/* Local variable */
 	e_eFSS_BLOBC_RES l_eRes;
     e_eFSS_COREHL_RES l_eResHL;
 
-    /* Local variable for calculation */
+    /* Local var used for calculation */
     bool_t l_bIsInit;
 
 	/* Check pointer validity */
-	if( NULL == p_ptCtx )
+	if( ( NULL == p_ptCtx ) || ( NULL == p_ptBuff ) )
 	{
 		l_eRes = e_eFSS_BLOBC_RES_BADPOINTER;
 	}
 	else
 	{
 		/* Check Init */
-        l_eResHL = eFSS_DBC_IsInit(&p_ptCtx->tCOREHLCtx, &l_bIsInit);
-        l_eRes = eFSS_DB_DBCtoDBFLRes(l_eResHL);
+        l_eResHL = eFSS_COREHL_IsInit(&p_ptCtx->tCOREHLCtx, &l_bIsInit);
+        l_eRes = eFSS_LOGC_HLtoLOGCRes(l_eResHL);
 
         if( e_eFSS_BLOBC_RES_OK == l_eRes )
         {
@@ -141,13 +136,14 @@ e_eFSS_BLOBC_RES eFSS_BLOBC_GetStorageStatus(t_eFSS_BLOBC_Ctx* const p_ptCtx)
             else
             {
                 /* Check internal status validity */
-                if( false == eFSS_DB_IsStatusStillCoherent(p_ptCtx) )
+                if( false == eFSS_BLOBC_IsStatusStillCoherent(p_ptCtx) )
                 {
                     l_eRes = e_eFSS_BLOBC_RES_CORRUPTCTX;
                 }
                 else
                 {
-                    l_eRes = eFSS_BLOBC_OriginBackupAligner(p_ptCtx);
+                    l_eResHL = eFSS_COREHL_GetBuff(&p_ptCtx->tCOREHLCtx, p_ptBuff);
+                    l_eRes = eFSS_LOGC_HLtoLOGCRes(l_eResHL);
                 }
             }
         }
@@ -156,314 +152,7 @@ e_eFSS_BLOBC_RES eFSS_BLOBC_GetStorageStatus(t_eFSS_BLOBC_Ctx* const p_ptCtx)
 	return l_eRes;
 }
 
-e_eFSS_BLOBC_RES eFSS_BLOBC_GetInfo(t_eFSS_BLOBC_Ctx* const p_ptCtx, uint32_t* p_puBlobSize)
-{
-	/* Local return variable */
-	e_eFSS_BLOBC_RES l_eRes;
-    e_eFSS_COREHL_RES l_eResHL;
 
-    /* Local variable for calculation */
-    bool_t l_bIsInit;
-
-	/* Check pointer validity */
-	if( NULL == p_ptCtx )
-	{
-		l_eRes = e_eFSS_BLOBC_RES_BADPOINTER;
-	}
-	else
-	{
-		/* Check Init */
-        l_eResHL = eFSS_DBC_IsInit(&p_ptCtx->tCOREHLCtx, &l_bIsInit);
-        l_eRes = eFSS_DB_DBCtoDBFLRes(l_eResHL);
-
-        if( e_eFSS_BLOBC_RES_OK == l_eRes )
-        {
-            if( false == l_bIsInit )
-            {
-                l_eRes = e_eFSS_BLOBC_RES_NOINITLIB;
-            }
-            else
-            {
-                /* Check internal status validity */
-                if( false == eFSS_DB_IsStatusStillCoherent(p_ptCtx) )
-                {
-                    l_eRes = e_eFSS_BLOBC_RES_CORRUPTCTX;
-                }
-                else
-                {
-                    /* Fix any memory problem */
-                    l_eRes = eFSS_BLOBC_OriginBackupAligner(p_ptCtx);
-
-                    if( ( e_eFSS_BLOBC_RES_OK == l_eRes ) || ( e_eFSS_BLOBC_RES_OK_BKP_RCVRD == l_eRes ) )
-                    {
-                        /* Get the last page of the memory area and read the parameter uPageUseSpecific1 */
-                        /* Do no return if write is open */
-                    }
-                }
-            }
-        }
-	}
-
-	return l_eRes;
-}
-
-e_eFSS_BLOBC_RES eFSS_BLOBC_Format(t_eFSS_BLOBC_Ctx* const p_ptCtx)
-{
-	/* Local return variable */
-	e_eFSS_BLOBC_RES l_eRes;
-    e_eFSS_COREHL_RES l_eResHL;
-
-    /* Local variable for calculation */
-    bool_t l_bIsInit;
-
-	/* Check pointer validity */
-	if( NULL == p_ptCtx )
-	{
-		l_eRes = e_eFSS_BLOBC_RES_BADPOINTER;
-	}
-	else
-	{
-		/* Check Init */
-        l_eResHL = eFSS_DBC_IsInit(&p_ptCtx->tCOREHLCtx, &l_bIsInit);
-        l_eRes = eFSS_DB_DBCtoDBFLRes(l_eResHL);
-
-        if( e_eFSS_BLOBC_RES_OK == l_eRes )
-        {
-            if( false == l_bIsInit )
-            {
-                l_eRes = e_eFSS_BLOBC_RES_NOINITLIB;
-            }
-            else
-            {
-                /* Check internal status validity */
-                if( false == eFSS_DB_IsStatusStillCoherent(p_ptCtx) )
-                {
-                    l_eRes = e_eFSS_BLOBC_RES_CORRUPTCTX;
-                }
-                else
-                {
-                    /* write at zero every page and bakup pages, do no use the backuped pages function,
-                       first write all original pages, and only after the backup one */
-                }
-            }
-        }
-	}
-
-	return l_eRes;
-}
-
-e_eFSS_BLOBC_RES eFSS_BLOBC_ReadBlob(t_eFSS_BLOBC_Ctx* const p_ptCtx, uint32_t p_uOffset, uint8_t* p_puBuff,
-                                     uint32_t p_uBuffL)
-{
-	/* Local return variable */
-	e_eFSS_BLOBC_RES l_eRes;
-    e_eFSS_COREHL_RES l_eResHL;
-
-    /* Local variable for calculation */
-    bool_t l_bIsInit;
-
-	/* Check pointer validity */
-	if( NULL == p_ptCtx )
-	{
-		l_eRes = e_eFSS_BLOBC_RES_BADPOINTER;
-	}
-	else
-	{
-		/* Check Init */
-        l_eResHL = eFSS_DBC_IsInit(&p_ptCtx->tCOREHLCtx, &l_bIsInit);
-        l_eRes = eFSS_DB_DBCtoDBFLRes(l_eResHL);
-
-        if( e_eFSS_BLOBC_RES_OK == l_eRes )
-        {
-            if( false == l_bIsInit )
-            {
-                l_eRes = e_eFSS_BLOBC_RES_NOINITLIB;
-            }
-            else
-            {
-                /* Check internal status validity */
-                if( false == eFSS_DB_IsStatusStillCoherent(p_ptCtx) )
-                {
-                    l_eRes = e_eFSS_BLOBC_RES_CORRUPTCTX;
-                }
-                else
-                {
-                    /* Fix any memory problem */
-                    l_eRes = eFSS_BLOBC_OriginBackupAligner(p_ptCtx);
-
-                    if( ( e_eFSS_BLOBC_RES_OK == l_eRes ) || ( e_eFSS_BLOBC_RES_OK_BKP_RCVRD == l_eRes ) )
-                    {
-                        /* Can return requested data if offset and size are coherent */
-                    }
-                }
-            }
-        }
-	}
-
-	return l_eRes;
-}
-
-e_eFSS_BLOBC_RES eFSS_BLOBC_StartWrite(t_eFSS_BLOBC_Ctx* const p_ptCtx)
-{
-	/* Local return variable */
-	e_eFSS_BLOBC_RES l_eRes;
-    e_eFSS_COREHL_RES l_eResHL;
-
-    /* Local variable for calculation */
-    bool_t l_bIsInit;
-
-	/* Check pointer validity */
-	if( NULL == p_ptCtx )
-	{
-		l_eRes = e_eFSS_BLOBC_RES_BADPOINTER;
-	}
-	else
-	{
-		/* Check Init */
-        l_eResHL = eFSS_DBC_IsInit(&p_ptCtx->tCOREHLCtx, &l_bIsInit);
-        l_eRes = eFSS_DB_DBCtoDBFLRes(l_eResHL);
-
-        if( e_eFSS_BLOBC_RES_OK == l_eRes )
-        {
-            if( false == l_bIsInit )
-            {
-                l_eRes = e_eFSS_BLOBC_RES_NOINITLIB;
-            }
-            else
-            {
-                /* Check internal status validity */
-                if( false == eFSS_DB_IsStatusStillCoherent(p_ptCtx) )
-                {
-                    l_eRes = e_eFSS_BLOBC_RES_CORRUPTCTX;
-                }
-                else
-                {
-                    /* Fix any memory problem */
-                    l_eRes = eFSS_BLOBC_OriginBackupAligner(p_ptCtx);
-
-                    if( ( e_eFSS_BLOBC_RES_OK == l_eRes ) || ( e_eFSS_BLOBC_RES_OK_BKP_RCVRD == l_eRes ) )
-                    {
-                        /* Can return requested data if offset and size are coherent */
-                        p_ptCtx->bIsWriteOnGoing = true;
-                        p_ptCtx->uDataWritten = 0u;
-                    }
-                }
-            }
-        }
-	}
-
-	return l_eRes;
-}
-
-e_eFSS_BLOBC_RES eFSS_BLOBC_AppendData(t_eFSS_BLOBC_Ctx* const p_ptCtx, uint8_t* p_puBuff, uint32_t p_uBuffL)
-{
-	/* Local return variable */
-	e_eFSS_BLOBC_RES l_eRes;
-    e_eFSS_COREHL_RES l_eResHL;
-
-    /* Local variable for calculation */
-    bool_t l_bIsInit;
-
-	/* Check pointer validity */
-	if( NULL == p_ptCtx )
-	{
-		l_eRes = e_eFSS_BLOBC_RES_BADPOINTER;
-	}
-	else
-	{
-		/* Check Init */
-        l_eResHL = eFSS_DBC_IsInit(&p_ptCtx->tCOREHLCtx, &l_bIsInit);
-        l_eRes = eFSS_DB_DBCtoDBFLRes(l_eResHL);
-
-        if( e_eFSS_BLOBC_RES_OK == l_eRes )
-        {
-            if( false == l_bIsInit )
-            {
-                l_eRes = e_eFSS_BLOBC_RES_NOINITLIB;
-            }
-            else
-            {
-                /* Check internal status validity */
-                if( false == eFSS_DB_IsStatusStillCoherent(p_ptCtx) )
-                {
-                    l_eRes = e_eFSS_BLOBC_RES_CORRUPTCTX;
-                }
-                else
-                {
-                    /* Fix any memory problem */
-                    l_eRes = eFSS_BLOBC_OriginBackupAligner(p_ptCtx);
-
-                    if( ( e_eFSS_BLOBC_RES_OK == l_eRes ) || ( e_eFSS_BLOBC_RES_OK_BKP_RCVRD == l_eRes ) )
-                    {
-                        /* Load the needed page */
-                        /* Write data needed (only of the original pages) */
-                        /* DO NO GENERATE the backup pages */
-                        /* Clear context */
-                        p_ptCtx->bIsWriteOnGoing = false;
-                        p_ptCtx->uDataWritten += p_uBuffL;
-                    }
-                }
-            }
-        }
-	}
-
-	return l_eRes;
-}
-
-e_eFSS_BLOBC_RES eFSS_BLOBC_EndWrite(t_eFSS_BLOBC_Ctx* const p_ptCtx)
-{
-	/* Local return variable */
-	e_eFSS_BLOBC_RES l_eRes;
-    e_eFSS_COREHL_RES l_eResHL;
-
-    /* Local variable for calculation */
-    bool_t l_bIsInit;
-
-	/* Check pointer validity */
-	if( NULL == p_ptCtx )
-	{
-		l_eRes = e_eFSS_BLOBC_RES_BADPOINTER;
-	}
-	else
-	{
-		/* Check Init */
-        l_eResHL = eFSS_DBC_IsInit(&p_ptCtx->tCOREHLCtx, &l_bIsInit);
-        l_eRes = eFSS_DB_DBCtoDBFLRes(l_eResHL);
-
-        if( e_eFSS_BLOBC_RES_OK == l_eRes )
-        {
-            if( false == l_bIsInit )
-            {
-                l_eRes = e_eFSS_BLOBC_RES_NOINITLIB;
-            }
-            else
-            {
-                /* Check internal status validity */
-                if( false == eFSS_DB_IsStatusStillCoherent(p_ptCtx) )
-                {
-                    l_eRes = e_eFSS_BLOBC_RES_CORRUPTCTX;
-                }
-                else
-                {
-                    /* Fix any memory problem */
-                    l_eRes = eFSS_BLOBC_OriginBackupAligner(p_ptCtx);
-
-                    if( ( e_eFSS_BLOBC_RES_OK == l_eRes ) || ( e_eFSS_BLOBC_RES_OK_BKP_RCVRD == l_eRes ) )
-                    {
-                        /* Calculate the CRC of the written data */
-                        /* Save on the last page CRC and size of the data written (only of the original pages) */
-                        /* Generate the backup pages */
-                        /* Clear context */
-                        p_ptCtx->bIsWriteOnGoing = false;
-                        p_ptCtx->uDataWritten = 0u;
-                    }
-                }
-            }
-        }
-	}
-
-	return l_eRes;
-}
 
 /***********************************************************************************************************************
  *  PRIVATE FUNCTION
